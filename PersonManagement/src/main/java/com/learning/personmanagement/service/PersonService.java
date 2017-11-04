0733dc57bb6f612d81446eea80bf55e1e75b142a
ravichavali9 <ravi.ileyana@gmail.com>
2017-11-04 17:55:51 -0400

    commiting add service

--- PersonManagement/src/main/java/com/learning/personmanagement/service/PersonService.java
+++ PersonManagement/src/main/java/com/learning/personmanagement/service/PersonService.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.personmanagement.PersonManagementApplication;
@@ -12,8 +13,15 @@ import com.learning.personmanagement.model.Person;
 @RestController
 @RequestMapping(value="/rest/person")
 public class PersonService {
-	@RequestMapping(value="/",method= RequestMethod.GET)
+	@RequestMapping(value="/",method = RequestMethod.GET)
 	public Map<Long,Person> getAllPersons(){
 		return PersonManagementApplication.personRecord;
+	}
+	
+	@RequestMapping(value="/add",method = RequestMethod.POST)
+	public Person addPerson(@RequestParam(value="name") String personName,@RequestParam(value="id") long personId,@RequestParam(value="address") String personAddress){
+		Person fellow = new Person(personName,personId,personAddress);
+		PersonManagementApplication.personRecord.put(new Long(fellow.getPersonId()),fellow);
+		return fellow;
 	}
 }
